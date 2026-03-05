b4d4ee87b8b80a42bdbcfe0e69a0e59f7696da1e
ethanlauy <160802780+ethanlauy@users.noreply.github.com>
2026-03-05 20:29:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,57 @@
+import streamlit as st
+import pandas as pd
 
+st.set_page_config(page_title="WA Information Portal", page_icon="📚")
+
+st.title("2026 WA Information Portal")
+
+st.write("Parents can check their child's WA dates and topics.")
+
+st.divider()
+
+# Load Excel file
+df = pd.read_excel("wa_information_2026.xlsx")
+
+# Convert index numbers
+df["Index Number"] = df["Index Number"].astype(int)
+
+# User inputs
+class_input = st.selectbox(
+    "Select Class",
+    sorted(df["Class"].unique())
+)
+
+index_input = st.number_input(
+    "Enter Index Number",
+    min_value=1,
+    max_value=40,
+    step=1
+)
+
+if st.button("Check WA Information"):
+
+    student = df[
+        (df["Class"] == class_input) &
+        (df["Index Number"] == index_input)
+    ]
+
+    if not student.empty:
+
+        row = student.iloc[0]
+
+        st.subheader("WA Schedule")
+
+        st.write("English WA:", row["English WA"])
+        st.write("Topic:", row["English WA Topic"])
+
+        st.write("Math WA:", row["Maths WA"])
+        st.write("Topic:", row["Maths WA Topic"])
+
+        st.write("Science WA:", row["Science WA"])
+        st.write("Topic:", row["Science WA Topic"])
+
+        st.write("Mother Tongue WA:", row["Mother Tongue WA"])
+        st.write("Topic:", row["Mother Tongue WA Topic"])
+
+    else:
+        st.error("Student not found.")
